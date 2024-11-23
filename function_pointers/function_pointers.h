74aa8229d7ad36c46c7db48ea565077d4d350d08
Gabriel-Nct <bescondgabriel@gmail.com>
2024-11-23 12:03:01 +0100

    Include functions_pointers.h manquant

--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -1,7 +1,7 @@
 #ifndef FUNCTION_POINTERS_H
 #define FUNCTION_POINTERS_H
 #include <stddef.h>
-/* Déclaration de Prototype*/void print_name(char *name, void (*f)(char *));
+void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
 #endif
