b37ea38b84d584f7a9e92cd0316fb1dfb64987e9
Gabriel-Nct <bescondgabriel@gmail.com>
2024-11-23 11:20:03 +0100

    Modification des commmentaires

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 /*
- * La fonction pritn_name qui prend une chaîne de caractère name et applique une opération spécifiée
- * via un pointeur de fonction *f qui va faire appelle 
- * à une fonction qui prend en sortie void et en entrée une chaîne de caractère
+ * La fonction print_name qui prend une chaîne de caractère name
+ * et applique une opération spécifiée via un pointeur de fonction *f
+ * qui va faire appelle à une fonction qui prend en sortie void
+ * et en entrée une chaîne de caractère
  */
 void print_name(char *name, void (*f)(char *))
 {
