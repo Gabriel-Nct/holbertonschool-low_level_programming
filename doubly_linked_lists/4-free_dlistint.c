4e51f3e12fb1af166e6568d66078a92d245b7acd
Gabriel-Nct <bescondgabriel@gmail.com>
2024-12-07 14:48:45 +0100

    Added Task 4

---
+++ doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "lists.h"
+/**
+ * free_list - Libère tous les nœuds d'une liste list_t
+ * @head: Pointeur vers le premier nœud de la liste
+ */
+void free_dlistint(dlistint_t *head)
+{
+dlistint_t *temporaire;    
+while (head != NULL)
+{
+temporaire = head->next;
+free(head);
+head = temporaire;
+}
+}
