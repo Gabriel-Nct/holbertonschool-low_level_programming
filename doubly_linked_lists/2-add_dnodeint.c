f8d1c870c0512accf84c5bee5908a19f3eb3c914
Gabriel-Nct <bescondgabriel@gmail.com>
2024-12-07 14:12:36 +0100

    Added Task 2

---
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include "lists.h"
+/**
+ * add_dnodeint - Ajoute un nouveau noeud au début de la liste
+ * @head: Le pointer du premier noeud de la liste
+ * 
+ * Return: La nouvelle liste, ou NULL si cela échoue.
+ */
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+dlistint_t *debutliste;
+
+    debutliste = malloc(sizeof(dlistint_t));
+    if (debutliste == NULL)
+        return (NULL);
+    debutliste->n = n;
+    debutliste->next = *head;
+    debutliste->prev = NULL;
+if (*head != NULL)
+    (*head)->prev = debutliste;
+*head = debutliste;
+ return (debutliste);
+}
