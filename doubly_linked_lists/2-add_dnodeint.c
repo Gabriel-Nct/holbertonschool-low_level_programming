31c8e9c69d99dceddca593e0b321c2b4d97e83dc
Gabriel-Nct <bescondgabriel@gmail.com>
2024-12-07 14:16:16 +0100

    Fixed with Betty

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -5,21 +5,20 @@
 /**
  * add_dnodeint - Ajoute un nouveau noeud au début de la liste
  * @head: Le pointer du premier noeud de la liste
- * 
  * Return: La nouvelle liste, ou NULL si cela échoue.
+ * @n: l'entier stocké dans le noeud à remplacer par l'entier en entrée.
  */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 dlistint_t *debutliste;
-
-    debutliste = malloc(sizeof(dlistint_t));
-    if (debutliste == NULL)
-        return (NULL);
-    debutliste->n = n;
-    debutliste->next = *head;
-    debutliste->prev = NULL;
+debutliste = malloc(sizeof(dlistint_t));
+if (debutliste == NULL)
+return (NULL);
+debutliste->n = n;
+debutliste->next = *head;
+debutliste->prev = NULL;
 if (*head != NULL)
-    (*head)->prev = debutliste;
+(*head)->prev = debutliste;
 *head = debutliste;
- return (debutliste);
+return (debutliste);
 }
