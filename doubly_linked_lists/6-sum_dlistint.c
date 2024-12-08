5a0bbd95d1234d9385a24caea0014c17c677c8c8
Gabriel-Nct <bescondgabriel@gmail.com>
2024-12-08 16:27:41 +0100

    Add Task 6

---
+++ doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+/**
+ * sum_dlistint - renvoie la somme de toutes les données dans dlistint_t.
+ * @head: pointer sur la tête de la liste
+ * Return: la somme de toutes les données, ou 0 si la liste est vide.
+ */
+int sum_dlistint(dlistint_t *head)
+{
+int somme = 0;
+while (head != NULL)
+{
+somme += head->n;
+head = head->next;
+}
+return somme;
+}
